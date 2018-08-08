a02b0f1d304e9961a6a06c70f2fb11b95fc45c9c
Luis <luis_lopez@dlsu.edu.ph>
2018-08-09 07:54:37 +0800

    Added sparse:true option in Schema with optional unique fields

--- model/Meme.js
+++ model/Meme.js
@@ -12,13 +12,14 @@ var MemeSchema = mongoose.Schema({
     trim : true
   },
   owner : {
-    user_id : {type : mongoose.Schema.Types.ObjectId, required : true, unique : true},
+    user_id : {type : mongoose.Schema.Types.ObjectId, required : true, unique : true, sparse : true},
     username: {
         type: String, //type == required property
         required: true,
         minlength: 6,
         trim: true, //remove whitespace
         unique: true,
+        sparse : true,
         collation: {
           locale: 'en_US',
           strength: 1
@@ -38,6 +39,7 @@ var MemeSchema = mongoose.Schema({
         minlength: 6,
         trim: true, //remove whitespace
         unique: true,
+        sparse : true,
         collation: {
           locale: 'en_US',
           strength: 1
