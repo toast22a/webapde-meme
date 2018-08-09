00161cef7e39929ab3d2d67d8190cf07dd02874f
Luis <luis_lopez@dlsu.edu.ph>
2018-08-09 08:44:32 +0800

    Added readUser

--- crud.js
+++ crud.js
@@ -24,6 +24,11 @@ let sampleCreateUserBody = {
   description : "programmer who dreams to be a programmer who can program the world"
 }
 
+let sampleReadUserBody = {
+  _id : "5b6b8646ec3bf1941dcfb7d7"
+  //username : "limeSapphire"
+}
+
 let sampleUpdateUserBody = {
   _id : "5b6b8646ec3bf1941dcfb7d7",
   description : "mochi mochi"
@@ -43,6 +48,24 @@ function createUser(body) {
   })
 }
 
+function readUser(body) {
+  if (body._id) {
+    let _id = body._id
+    User.findById(_id, "_id username description owned_memes", (err, doc)=>{
+      if (err) handleError(err)
+      else if (doc) console.log(JSON.stringify(doc))
+      else console.log("User@" + _id + " not found")
+    })
+  } else {
+    let username = body.username
+    User.findOne({username}, "_id username description owned_memes", (err, doc)=>{
+      if (err) handleError(err)
+      else if (doc) console.log(JSON.stringify(doc))
+      else console.log("User " + username + " not found")
+    }).collation({locale : "en_US", strength : 1})
+  }
+}
+
 function updateUser(body) {
   let _id = body._id
   let toUpdate = {}
@@ -54,4 +77,4 @@ function updateUser(body) {
   })
 }
 
-updateUser(sampleUpdateUserBody)
+readUser(sampleReadUserBody)
